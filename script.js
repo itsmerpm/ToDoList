3e751d6fca9aeff78910d66c1b3ded6838938000
itsmerpm <rahulpmtan91@gmail.com>
2023-08-05 19:42:49 +0530

    Small screen device optimization

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //Enter button to submit
 const input = document.querySelector(".textbox");
-const pressbtn = document.querySelector(".btn")
-input.addEventListener("keyup",(e)=>{
+const pressbtn = document.querySelector(".btn");
+
+input.addEventListener("keyup", (e) => {
     e.preventDefault();
-    if(e.keyCode === 13){
+    if (e.keyCode === 13) {
         pressbtn.click();
     }
-
-})
+});
 //main code -- Adding Tasks
 document.querySelector(".btn").onclick = function () {
     if (document.querySelector(".textbox").value.length == 0) {
@@ -19,7 +19,7 @@ document.querySelector(".btn").onclick = function () {
         const span = document.createElement("span");
         const editBtn = document.createElement("button");
         editBtn.classList.add("sp1");
-        editBtn.innerHTML = '<i class="fa-solid fa-edit"></i>'
+        editBtn.innerHTML = '<i class="fa-solid fa-edit"></i>';
         li.innerText = inputbox.value;
         ul.appendChild(li);
         span.innerHTML = '<i class="fa-solid fa-check"></i>';
@@ -27,18 +27,17 @@ document.querySelector(".btn").onclick = function () {
         li.appendChild(editBtn);
     }
     // edit btn
-    const deletebtn = document.querySelectorAll(".sp1")
-    let li = document.querySelector("li")
-    for (let i = 0; i < deletebtn.length; i++){
-        deletebtn[i].addEventListener("click",()=>{
+    const deletebtn = document.querySelectorAll(".sp1");
+    let li = document.querySelector("li");
+    for (let i = 0; i < deletebtn.length; i++) {
+        deletebtn[i].addEventListener("click", () => {
             document.querySelector(".textbox").value = deletebtn[i].parentNode.innerText;
             document.querySelector(".textbox").focus();
             deletebtn[i].parentElement.style.opacity = 0;
             setTimeout(() => {
                 deletebtn[i].parentElement.remove();
             }, 200);
-            
-        })
+        });
     }
     // Tick button
     const close = document.querySelectorAll("span");
@@ -51,30 +50,22 @@ document.querySelector(".btn").onclick = function () {
         });
     }
 
-  
-//clear all button 
+    //clear all button
     const clearallbtn = document.querySelector("#clearallbtn");
     for (let i = 0; i < close.length; i++) {
         clearallbtn.addEventListener("click", () => {
             close[i].parentElement.style.opacity = 0;
             setTimeout(() => {
-                clearallbtn.style.display = "none"
+                clearallbtn.style.display = "none";
                 close[i].parentElement.remove();
             }, 500);
-            
         });
-       
     }
     //clear button visibility
-    if(close){
+    if (close) {
         clearallbtn.style.display = "block";
     }
-    
 
-
-
-   //empty input box after enter
+    //empty input box after enter
     document.querySelector(".textbox").value = "";
 };
-
-
